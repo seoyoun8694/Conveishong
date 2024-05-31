0a5f0ea8efa2650631b7a1c5edd154170638d0fe
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-06-01 03:25:18 +0900

    React-Native New Project

--- frontend/babel.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	presets: ['metro-react-native-babel-preset'],
-	plugins: ['@babel/plugin-transform-private-methods'],
-};  
--- frontend/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('@expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-
-defaultConfig.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
-defaultConfig.resolver.sourceExts = [...defaultConfig.resolver.sourceExts, 'svg'];
-
-module.exports = defaultConfig;
