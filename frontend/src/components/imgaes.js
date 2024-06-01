2c9c00b557795f61265a357b7019ecffaf815fc1
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-06-02 04:34:46 +0900

    SVG 파일 사용 세팅
    metro.config.js + noe_modules(react-native-svg, react-native-svg-transformer 추가)

---
+++ frontend/src/components/imgaes.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable quotes */
+
+import Back_icon from '../assets/back_icon.svg';
+import PartTime_Register from '../assets/PartTime_Register.svg';
+import President_Register from '../assets/President_Register.svg';
+
+export const images = {
+	Back_icon: Back_icon,
+	PartTime_Register: PartTime_Register,
+	President_Register: President_Register,
+};
+
+export default images;
--- frontend/src/pages/Register1.js
+++ frontend/src/pages/Register1.js
@@ -2,18 +2,47 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable react/self-closing-comp */
 
-import React from 'react';
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import {
+	View,
+	ScrollView,
+	TouchableOpacity,
+	Alert,
+	Text,
+	Image,
+} from "react-native";
 import { useNavigation } from '@react-navigation/native';
-import styled, { ThemeProvider } from "styled-components/native"
+import styled from "styled-components/native"
+
+import images from '../components/imgaes';
 
 function Register1({}) {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
+	const user_name = '홍길동';
+	const [selected, setSelected] = useState(null);
+
+	const handleImageClick = (type) => {
+		setSelected(type);
+	};
 
 	return (
 		<FullView>
+			<images.Back_icon style={{ marginTop: 30, marginLeft: 20 }} />
+			<View style={{ flexDirection: 'row' }}>
+				<Bar style={{ backgroundColor: '#0066FF', width: '25%' }} />
+				<Bar />
+			</View>
+			
 			<MainView>
-				<MainText>회원가입 화면1</MainText>
+				<MainText style={{ marginTop: 20 }}>안녕하세요 {user_name}님, {'\n'} 어떤 일을 하고 계신가요?</MainText>
+				<View style={{ alignItems: 'center', justifyContent: 'center' }}>
+					<TouchableOpacity onPress={() => handleImageClick('partTime')}>
+						<images.PartTime_Register />
+					</TouchableOpacity>
+					<TouchableOpacity onPress={() => handleImageClick('president')}>
+						<images.President_Register />
+					</TouchableOpacity>
+				</View>
 			</MainView>
 		</FullView>
 	);
@@ -24,19 +53,24 @@ const FullView = styled.View`
 	height: 100%;
 	background-color: white;
 	align-self: center;
-	justify-content: center;
 `;
 
 const MainView = styled(FullView)`
-	align-self: center;
-	justify-content: center;
+	width: 80%;
+	height: auto;
 `;
 
 const MainText = styled.Text`
-    font-size: 20px;
+    font-size: 15px;
     font-weight: bold;
     color: ${props => props.color || "black"};
-    text-align: center;
+`;
+
+const Bar = styled.View`
+	width: 100%;
+	height: 1.5px;
+	background-color: ${props => props.style?.backgroundColor || "#D9D9D9"};
+	margin-top: 20px;
 `;
 
 export default Register1;
