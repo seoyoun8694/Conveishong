f369314625b392d0cbb0795c4b775438de36f5dc
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-06-02 01:03:52 +0900

    Naviagion + 기본 node_modules 설치

--- frontend/App.js
+++ frontend/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+/* eslint-disable prettier/prettier */
 
-const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello, React Native!</Text>
-    </SafeAreaView>
-  );
-};
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import 'react-native-gesture-handler';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-  },
-});
+import Login from "./src/pages/Login";
+import Register1 from "./src/pages/Register1";
 
-export default App;
+const Stack = createStackNavigator();
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="Login" component={Login} />
+				<Stack.Screen name="Register1" component={Register1} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
