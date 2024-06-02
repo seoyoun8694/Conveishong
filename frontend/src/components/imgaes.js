1286ceb589d52447b603f0d9228ae0ab60cf8038
seoyoun8694 <102515499+seoyoun8694@users.noreply.github.com>
2024-06-02 15:17:36 +0900

    Register1 page design

--- frontend/src/components/imgaes.js
+++ frontend/src/components/imgaes.js
@@ -2,13 +2,19 @@
 /* eslint-disable quotes */
 
 import Back_icon from '../assets/back_icon.svg';
-import PartTime_Register from '../assets/PartTime_Register.svg';
-import President_Register from '../assets/President_Register.svg';
+import Register_PT from '../assets/Register_PT.png';
+import Register_P from '../assets/Register_P.png';
+import Register_PT1 from '../assets/Register_PT1.png';
+import Register_P1 from '../assets/Register_P1.png';
+import Register_circle from '../assets/Register_circle.svg';
 
 export const images = {
 	Back_icon: Back_icon,
-	PartTime_Register: PartTime_Register,
-	President_Register: President_Register,
+	Register_PT: Register_PT,
+	Register_P: Register_P,
+	Register_PT1: Register_PT1,
+	Register_P1: Register_P1,
+	Register_circle: Register_circle,
 };
 
 export default images;
--- frontend/src/pages/Register1.js
+++ frontend/src/pages/Register1.js
@@ -5,21 +5,18 @@
 import React, { useState } from 'react';
 import {
 	View,
-	ScrollView,
 	TouchableOpacity,
-	Alert,
 	Text,
-	Image,
 } from "react-native";
 import { useNavigation } from '@react-navigation/native';
-import styled from "styled-components/native"
+import styled from "styled-components/native";
 
 import images from '../components/imgaes';
 
 function Register1({}) {
 	const navigation = useNavigation();
 	const user_name = '홍길동';
-	const [selected, setSelected] = useState(null);
+	const [selected, setSelected] = useState('partTime');
 
 	const handleImageClick = (type) => {
 		setSelected(type);
@@ -35,15 +32,36 @@ function Register1({}) {
 			
 			<MainView>
 				<MainText style={{ marginTop: 20 }}>안녕하세요 {user_name}님, {'\n'} 어떤 일을 하고 계신가요?</MainText>
-				<View style={{ alignItems: 'center', justifyContent: 'center' }}>
-					<TouchableOpacity onPress={() => handleImageClick('partTime')}>
-						<images.PartTime_Register />
+				<View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 40 }}>
+					<MainText>알바생</MainText>
+					<TouchableOpacity
+						style={{ marginBottom: 40 }}
+						onPress={() => handleImageClick('partTime')}
+					>
+						<StyledImage
+							source={selected === 'partTime' ? images.Register_PT : images.Register_PT1}
+						/>
 					</TouchableOpacity>
+
+					<MainText>점주</MainText>
 					<TouchableOpacity onPress={() => handleImageClick('president')}>
-						<images.President_Register />
+						<StyledImage
+							source={selected === 'president' ? images.Register_P : images.Register_P1}
+						/>
 					</TouchableOpacity>
 				</View>
 			</MainView>
+
+			<View style={{ flexDirection: 'row', alignSelf: 'center', marginTop: 50}}>
+				<ResultCircle color={'#0066FF'} style={{width: 15}} />
+				<ResultCircle />
+				<ResultCircle />
+				<ResultCircle/>
+			</View>
+
+			<ResultButton>
+				<ResultButtonText>다음</ResultButtonText>
+			</ResultButton>
 		</FullView>
 	);
 }
@@ -71,6 +89,36 @@ const Bar = styled.View`
 	height: 1.5px;
 	background-color: ${props => props.style?.backgroundColor || "#D9D9D9"};
 	margin-top: 20px;
+`;
+
+const StyledImage = styled.Image`
+	width: 250px;
+	height: 146px;
+	margin: 10px;
+`;
+
+const ResultButton = styled.TouchableOpacity`
+	width: 100%;
+	height: 70px;
+	background-color: #0066FF;
+	position: absolute;
+	bottom: 0px;
+	justify-content: center;
+	align-items: center;
+`;
+
+const ResultButtonText = styled.Text`
+	color: white;
+	font-size: 20px;
+	font-weight: bold;
+`;
+
+const ResultCircle = styled.View`
+	width: 5px;
+	height: 5px;
+	margin: 2px;
+	background-color: ${props => props.color || "#D9D9D9"};
+	border-radius: 100px;
 `;
 
 export default Register1;
