3b860a17fa10171248900ee5f78c4e713028ee34
SeongEun <bongfam2@naver.com>
2024-07-06 02:54:31 +0900

    Auto stash before merge of "SeongEun" and "origin/SeongEun"

--- backend/src/main/java/com/example/Conveishong/Dto/StackDTO.java
+++ backend/src/main/java/com/example/Conveishong/Dto/StackDTO.java
@@ -1,2 +1,18 @@
+<<<<<<< Updated upstream
 package com.example.Conveishong.Dto;public class StackDTO {
+=======
+package com.example.Conveishong.Dto;
+
+import lombok.Data;
+
+@Data
+public class StackDTO {
+    private Long stackId;
+    private Long marketId;
+    private String stackName;
+    private Long stackNum;
+    private String stackType;
+    private String stackDay;
+    private String userId;
+>>>>>>> Stashed changes
 }
